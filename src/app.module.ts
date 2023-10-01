7d6cb21413ef856a46956694fc4537dba13d3b26
nt4rever <levantanald@gmail.com>
2023-10-01 11:33:33 +0700

    feat: add joi validation env

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,34 @@ import { APP_FILTER } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GlobalExceptionFilter } from './exception-filters/global-exception.filter';
 import { AwsModule } from '@modules/aws/aws.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_URI: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+        REDIS_HOST: Joi.string().required(),
+        JWT_ACCESS_TOKEN_PRIVATE_KEY: Joi.string().required(),
+        JWT_REFRESH_TOKEN_PRIVATE_KEY: Joi.string().required(),
+        JWT_ACCESS_TOKEN_EXPIRATION_TIME: Joi.number().required(),
+        JWT_REFRESH_TOKEN_EXPIRATION_TIME: Joi.number().required(),
+        AWS_ENDPOINT: Joi.string().required(),
+        AWS_S3_BUCKET: Joi.string().required(),
+        AWS_S3_REGION: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+      }),
+      validationOptions: {
+        abortEarly: false,
+      },
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
       load: [database_config],
